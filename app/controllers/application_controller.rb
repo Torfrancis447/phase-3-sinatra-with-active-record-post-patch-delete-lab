f0fc5ca504b6ff2409d9d643faa3ea5f705a0598
tor <torfrancis447@gmail.com>
2023-01-03 10:39:57 -0500

    commit tree 84ded4803d88a937f41ed21ebc5b762040a18d59

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
   get '/bakeries' do
-    bakeries = Bakery.all
-    bakeries.to_json
+    bakeries = Bakery.all 
   end
   
   get '/bakeries/:id' do
@@ -20,6 +19,26 @@ class ApplicationController < Sinatra::Base
   get '/baked_goods/most_expensive' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
+  post '/baked_goods' do
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(name: params[:name])
+    bakery.to_json
+  end
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
     baked_good.to_json
   end
 
